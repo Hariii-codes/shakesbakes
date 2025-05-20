99e93869863f9de69483510aa3e1578dfcad8504
Hariii-codes <harishchawan11@gmail.com>
2025-05-21 00:26:00 +0530

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()]
+  build: {
+    chunkSizeWarningLimit: 2000  // Increase limit to 2000 KB (2 MB)
+  }
 });
